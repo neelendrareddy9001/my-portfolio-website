8ae50b143515fb7d304364d3bb18b59193f165af
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-03 22:59:36 +0200

    update all

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="nav_section" id="nav_section">
-        <h1>Hello World</h1>
+        <a className="icon_section" id="icon_section">
+          Neelendrareddy
+        </a>
+        <ul className="ul_menu_section">
+          <li>Home</li>
+          <li>About</li>
+          <li>Project</li>
+          <li>cv</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </>
   );
