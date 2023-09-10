68d712c0719418ca32a4232dfd1781969b349b17
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-10 23:05:00 +0200

    navbar with media updated

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,60 +1,23 @@
 import "./navbar.scss";
 
 import { Link } from "react-router-dom";
-
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Home from "../Home/Home";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
     <>
-      {/* <nav className="nav_section" id="nav_section">
-        <a href="/" className="icon_section" id="icon_section">
+      <div className="nav_section" id="nav_section">
+        <span href="/" className="icon_section_title" id="icon_section">
           Neelendrareddy
-        </a>
-        <ul className="ul_menu_section">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">About</a>
-          </li>
-          <li>
-            <a href="/">Project</a>
-          </li>
-          <li>
-            <a href="/">Cv</a>
-          </li>
-          <li>
-            <a href="/">Project</a>
-          </li>
-        </ul>
+        </span>
+        <nav className="nav_menu_section">
+          <a href="/about" className="nav_menu_navigation_link" />
+          <a href="/cv" className="nav_menu_navigation_link" />
+          <a href="/project" className="nav_menu_navigation_link" />
+          <a href="/contact" className="nav_menu_navigation_link" />
+        </nav>
         <RxHamburgerMenu size={38} className="nav_icon" />
-      </nav> */}
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <ul>
-            <li>
-              <Link to="/">
-                <Home />
-              </Link>
-            </li>
-          </ul>
-        </Toolbar>
-      </AppBar>
+      </div>
     </>
   );
 };
