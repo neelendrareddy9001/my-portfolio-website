ac23287c157c431a8fe4bf87cb8edada7c40bbce
neelendrareddy9001 <neelendrareddy123@gamil.com>
2023-09-26 22:26:09 +0200

    updated

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -12,13 +12,19 @@ const Navbar = () => {
         <span href="/" className="icon_section_title" id="icon_section">
           Neelendrareddy
         </span>
-        <nav className="nav_menu_section">
-          <a href="/about" className="nav_menu_navigation_link" />
-          <a href="/cv" className="nav_menu_navigation_link" />
-          <a href="/project" className="nav_menu_navigation_link" />
-          <a href="/contact" className="nav_menu_navigation_link" />
-        </nav>
-        <RxHamburgerMenu size={38} className="nav_icon" />
+        {showMenu && (
+          <nav className="nav_menu_section">
+            <a href="/about" className="nav_menu_navigation_link" />
+            <a href="/cv" className="nav_menu_navigation_link" />
+            <a href="/project" className="nav_menu_navigation_link" />
+            <a href="/contact" className="nav_menu_navigation_link" />
+          </nav>
+        )}
+        <RxHamburgerMenu
+          size={38}
+          className="nav_icon"
+          onClick={() => setShowMenu(!true)}
+        />
       </div>
     </>
   );
