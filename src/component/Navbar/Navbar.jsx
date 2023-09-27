1e455ad5ad5e5281d7b72942d8bc399d97c70699
neelendrareddy9001 <neelendrareddy123@gamil.com>
2023-09-27 23:28:31 +0200

    updated

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
   return (
     <>
       <div className="nav_section" id="nav_section">
-        <span href="/" className="icon_section_title" id="icon_section">
+        <span href="/" className="icon_section-title" id="icon_section">
           Neelendrareddy
         </span>
         {showMenu && (
-          <nav className="nav_menu_section">
-            <a href="/about" className="nav_menu_navigation_link" />
-            <a href="/cv" className="nav_menu_navigation_link" />
-            <a href="/project" className="nav_menu_navigation_link" />
-            <a href="/contact" className="nav_menu_navigation_link" />
+          <nav className="nav_menu-section">
+            <a href="/about" className="nav_menu-navigation-link" />
+            <a href="/cv" className="nav_menu-navigation-link" />
+            <a href="/project" className="nav_menu-navigation-link" />
+            <a href="/contact" className="nav_menu-navigation-link" />
           </nav>
         )}
         <RxHamburgerMenu
