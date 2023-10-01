46b8558c99058c0ed932525557abfc0d2a52e24c
neelendrareddy9001 <neeelendrareddy123@gmail.com>
2023-10-01 23:14:51 +0200

    updated about.js

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -7,8 +7,10 @@ const About = () => {
   return (
     <>
       <section className="about__container" id="about__container">
-        <div className="about__inner-container" id="about__inner-container">
-          <div className="about__left-container"></div>
+        <div className="about__container-inner" id="about__container-inner">
+          <div className="about__left-container">
+            <h3>My Portfolio</h3>
+          </div>
           <div className="about__right-container">
             <img src={myImg} alt="img" />
           </div>
