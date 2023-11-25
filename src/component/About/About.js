35df85179fa44c0241dbdc22394570c9093a0e6e
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-25 23:11:59 +0100

    html of about component has created

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./About.scss";
 
 import myImg from "../../assets/my_img.jpeg";
+import Info from "./Info";
 
 const About = () => {
   return (
@@ -9,7 +10,19 @@ const About = () => {
       <section className="about__section" id="about">
         <h2 className="section_title">About Me</h2>
         <span className="section_subtitle">My inroduction</span>
-        <div className="about_container container grid"></div>
+        <div className="about_container container grid">
+          <img src={myImg} className="about_img" alt="" />
+          <div className="about_data">
+            <Info />
+            <p className="about_description">
+              Since I am learning frontend development features. It motivates me
+              design multiple Ui templates as i kept on going.
+            </p>
+            <a download="" href="cv" className="button button_flex">
+              Download CV
+            </a>
+          </div>
+        </div>
       </section>
     </>
   );
