d0206d29f73d24602c6be2791855b69077d6efab
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-25 22:57:16 +0100

    updated

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.scss";
-import { Container } from "react-bootstrap";
 
 import myImg from "../../assets/my_img.jpeg";
 
@@ -8,7 +7,9 @@ const About = () => {
   return (
     <>
       <section className="about__section" id="about">
-        <div className="about__section-grid"></div>
+        <h2 className="section_title">About Me</h2>
+        <span className="section_subtitle">My inroduction</span>
+        <div className="about_container container grid"></div>
       </section>
     </>
   );
--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Contact.scss";
 
 const Contact = () => {
-  return <div className="contact">Contact</div>;
+  return <div>Contact</div>;
 };
 
 export default Contact;
