9fc5b9040c3b3243cf501fce67b111c8ea824799
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-25 23:18:30 +0100

    info component code written

--- src/component/About/Info.js
+++ src/component/About/Info.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <div className="about_info grid">
+      <div className="about_box">
+        <h3 className="about_title">Experience</h3>
+        <span className="about_subtitle">0 years of working</span>
+      </div>
+      <div className="about_box">
+        <h3 className="about_title">Completed</h3>
+        <span className="about_subtitle">Completed some basic ui projects</span>
+      </div>
+      <div className="about_box">
+        <h3 className="about_title">Support</h3>
+        <span className="about_subtitle">Always available on Social media</span>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
