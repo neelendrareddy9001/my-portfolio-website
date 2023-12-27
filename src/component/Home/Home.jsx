0b763cfd1955f01929c71723a1051102c2246705
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-27 23:47:17 +0100

    removed default styles

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -4,14 +4,7 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="layout__container">
-      <div className="layout__container-inner">
-        <div className="left">
-          <h1>hello</h1>
-        </div>
-        <div className="right">
-          <h1>World</h1>
-        </div>
-      </div>
+      <div className="layout__container-inner"></div>
     </div>
   );
 };
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -61,14 +61,7 @@ const Navbar = () => {
       </div> */}
 
       <div className="layout__container">
-        <div className="layout__container-inner">
-          <div className="left">
-            <h1>hello</h1>
-          </div>
-          <div className="right">
-            <h1>World</h1>
-          </div>
-        </div>
+        <div className="layout__container-inner"></div>
       </div>
     </>
   );
