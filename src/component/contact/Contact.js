59a8aa0dc3d609cc2f900fe3afa72003930b01cf
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-30 22:54:51 +0100

    conatact page upaated

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.scss";
 
 const Contact = () => {
-  return <div className="contacts-container">Contact</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="contacts-container">
+      <div className="contact_form_container">
+        <form onSubmit={handlerSubmit}>
+          <div className="input_text">
+            <label htmlFor="Name">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter name"
+              ></input>
+            </label>
+          </div>
+          <div className="input-text">
+            <label type="Email">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              >
+                Enter email
+              </input>
+            </label>
+          </div>
+          <div className="input-text">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            >
+              Enter pasowrd placeholder
+            </input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
