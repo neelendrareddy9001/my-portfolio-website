350f1539c55eef651127dcd600997c1400c72d74
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-01-01 21:35:21 +0100

    changing  style of contact page

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./Contact.scss";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -7,44 +10,36 @@ const Contact = () => {
   const [password, setPassword] = useState("");
   const handlerSubmit = () => {};
   return (
-    <div className="contacts-container">
-      <div className="contact_form_container">
-        <form onSubmit={handlerSubmit}>
-          <div className="input_text">
-            <label htmlFor="Name">
-              <input
-                type="text"
-        
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter name"
-              ></input>
-            </label>
+    <>
+      <section className="contact">
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+        <div className="container contact_container">
+          <div className="contact_options">
+            <article className="contact_option">
+              <MdOutlineEmail />
+              <h4>Email</h4>
+              <h5>neelendrareddy123@gmail.com</h5>
+              <a href="mailto:neelendrareddy123@gmail.com">Send me a message</a>
+            </article>
+            <article className="contact_option">
+              <RiMessengerLine />
+              <h4>Messenger</h4>
+              <h5>panditi.neelendrareddy</h5>
+              <a href="https://m.me/panditi.neelendrareddy.7">Send a message</a>
+            </article>
+            <article className="contact_opton">
+              <BsWhatsapp />
+              <h4>WhatsApp</h4>
+              <h5>+48739437031</h5>
+              <a href="https://api.whatsapp.com/send?+48739437031">
+                Send a message
+              </a>
+            </article>
           </div>
-          <div className="input-text">
-            <label type="Email">
-              <input
-                type="email"
-          
-                onChange={(e) => setEmail(e.target.value)}
-              >
-                Enter email
-              </input>
-            </label>
-          </div>
-          <div className="input-text">
-            <input
-              type="password"
-              
-              onChange={(e) => setPassword(e.target.value)}
-            >
-              Enter pasowrd placeholder
-            </input>
-          </div>
-        </form>
-      </div>
-      <button className="btn btn-primary">submt</button>
-    </div>
+        </div>
+      </section>
+    </>
   );
 };
-
 export default Contact;
