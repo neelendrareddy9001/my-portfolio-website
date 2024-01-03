77c93a82f7e00014a57a563395d45a981f9ca09a
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-01-03 23:25:07 +0100

    contact from is created

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -20,23 +20,59 @@ const Contact = () => {
               <MdOutlineEmail />
               <h4>Email</h4>
               <h5>neelendrareddy123@gmail.com</h5>
-              <a href="mailto:neelendrareddy123@gmail.com">Send me a message</a>
+              <a href="mailto:neelendrareddy123@gmail.com" target="_blank">
+                Send me a message
+              </a>
             </article>
             <article className="contact_option">
               <RiMessengerLine />
               <h4>Messenger</h4>
               <h5>panditi.neelendrareddy</h5>
-              <a href="https://m.me/panditi.neelendrareddy.7">Send a message</a>
+              <a href="https://m.me/panditi.neelendrareddy.7" target="_blank">
+                Send a message
+              </a>
             </article>
             <article className="contact_opton">
               <BsWhatsapp />
               <h4>WhatsApp</h4>
               <h5>+48739437031</h5>
-              <a href="https://api.whatsapp.com/send?+48739437031">
+              <a
+                href="https://api.whatsapp.com/send?+48739437031"
+                target="_blank"
+              >
                 Send a message
               </a>
             </article>
           </div>
+          {/* End of Contact Optons */}
+          <form action="">
+            <input
+              rel="norefereer"
+              value={name}
+              type="text"
+              name="name"
+              placeholder="Your Full name"
+              required
+            />
+            <input
+              rel="noreferer"
+              value={email}
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+            <textarea
+              rel="noreferer"
+              name="message"
+              rows="7"
+              placeholder="Your Message"
+              required
+            />
+            <button type="submit" className="btn btn-primary">
+              Sent a mesage
+            </button>
+          </form>
         </div>
       </section>
     </>
