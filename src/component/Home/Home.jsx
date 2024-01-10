5edaf53028af1847ed8df968de991c199d6601e6
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-01-10 23:22:31 +0100

    tsparticles and react-particles installed file craete

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./home.scss";
+import ParticleBg from "../ParticleBg";
 
 const Home = () => {
+  <ParticleBg />;
   return (
     <div className="layout__container">
       <div className="layout__container-inner"></div>
