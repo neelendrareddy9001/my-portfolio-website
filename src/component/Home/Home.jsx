9b05ac7dffbcd4750816f479e083ee5264a44482
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-01-22 06:04:58 +0100

    updated

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import "./home.scss";
 import ParticleBg from "../ParticleBg";
+import { Col, Container, Row } from "react-bootstrap";
+import Type from "../Navbar/Type";
 
 const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <section className="section">
+      <Container fluid className="home-section" id="home">
+        <ParticleBg />
+        <Container className="home-content">
+          <Row>
+            <Col md={7}>
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+              <h1 className="heading name">
+                I'am <strong className="main-name">NEELENDRAR EDDY</strong>
+              </h1>
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
 };
 
 export default Home;
