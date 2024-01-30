83d2b837fc2de3f429063bc1315420d85135d55b
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-01-30 23:37:05 +0100

    modified

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,73 +2,54 @@ import React, { useState } from "react";
 import "./navbar.scss";
 
 import { Link } from "react-router-dom";
-import { HiMenuAlt1, HiMenuAlt3 } from "react-icons/hi";
+
+import { BiMenu, BiX } from "react-icons/bi";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
   };
-
   return (
-    <div>
-      <div className="nav__layout" id="nav__layout">
-        <div className="nav__layout-center" id="nav__layout-center">
-          <header className="nav__layout-header" id="nav__layout-header">
-            {/* <Link to="/" style={{ textDecoration: "none" }}> */}
-            <Link to="/" className="nav__layout-title" id="nav__layout-title">
-              Neelendrareddy
-            </Link>
-
-            {/* </Link> */}
-            <ul className={showMenu ? "menu_open menu" : "menu"}>
-              <nav className="nav__layout-menu-section active">
-                <Link to="/about">
-                  <a
-                    href="/about"
-                    className={`nav__layout-navigation-link ${showMenu}`}
-                  >
-                    About
-                  </a>
-                </Link>
-
-                <Link to="/projects">
-                  <a href="/projects" className="nav__layout-navigation-link">
-                    Projects
-                  </a>
-                </Link>
-                <Link to="/cv">
-                  <a href="/cv" className="nav__layout-navigation-link">
-                    Cv
-                  </a>
-                </Link>
-                <Link to="/contact">
-                  <a href="/contact" className="nav__layout-navigation-link">
-                    contact
-                  </a>
-                </Link>
-              </nav>
-            </ul>
-
-            <div className="hamburger">
-              {showMenu ? (
-                <HiMenuAlt1
-                  className="nav__layout-icon"
-                  size={30}
-                  onClick={toggleMenu}
-                />
-              ) : (
-                <HiMenuAlt3
-                  className="nav__layout-icon"
-                  size={30}
-                  onClick={toggleMenu}
-                />
-              )}
-            </div>
-          </header>
-        </div>
+    <nav className={`header container`}>
+      <div className="logo">
+        <Link to="/">
+          <a href="/">
+            <h2>NeelendraReddy</h2>
+          </a>
+        </Link>
       </div>
-    </div>
+      <ul className={open ? "menu-open menu" : "menu"}>
+        <li className="menu-item">
+          <a href="/">
+            <h5>About</h5>
+          </a>
+        </li>
+        <li className="menu-item">
+          <a href="/about">
+            <h5>Cv</h5>
+          </a>
+        </li>
+        <li className="menu-item">
+          <a href="/portfolio">
+            <h5>Projects</h5>
+          </a>
+        </li>
+        <li className="menu-item">
+          <a href="/contact">
+            <h5>Contact</h5>
+          </a>
+        </li>
+      </ul>
+
+      <div className="hamburger">
+        {open ? (
+          <BiX onClick={handleClick} />
+        ) : (
+          <BiMenu onClick={handleClick} />
+        )}
+      </div>
+    </nav>
   );
 };
 
