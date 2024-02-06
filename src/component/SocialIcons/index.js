67f2a7bd8f4301481686a6cf3d65d45cdbeae484
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-02-06 03:49:44 +0100

    social media icons, and styles changes in the components

---
+++ src/component/SocialIcons/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.scss";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebook,
+  FaLinkedinIn,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+
+const index = () => {
+  return <div>index</div>;
+};
+
+export default index;
