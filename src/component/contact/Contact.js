1ee718b46bf2f6c4fff4a97fa6f91a1d17e8c061
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-02-17 23:22:03 +0100

    new styles added to contact options

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -19,24 +19,34 @@ const Contact = () => {
         <div className="container contact_container">
           <div className="contact_options">
             <article className="contact_option">
-              <MdOutlineEmail size={35} className="social_icon" />
-              <h4>Email</h4>
-              <h5>neelendrareddy123@gmail.com</h5>
-              <a href="mailto:neelendrareddy123@gmail.com">Send me a message</a>
+              <div className="contact_info">
+                <MdOutlineEmail size={35} className="social_icon" />
+                <h4>Email</h4>
+                <h5>neelendrareddy123@gmail.com</h5>
+                <a href="mailto:neelendrareddy123@gmail.com">
+                  Send me a message
+                </a>
+              </div>
             </article>
             <article className="contact_option">
-              <RiMessengerLine size={35} className="social_icon" />
-              <h4>Messenger</h4>
-              <h5>panditi.neelendrareddy</h5>
-              <a href="https://m.me/panditi.neelendrareddy.7">Send a message</a>
+              <div className="contact_info">
+                <RiMessengerLine size={35} className="social_icon" />
+                <h4>Messenger</h4>
+                <h5>panditi.neelendrareddy</h5>
+                <a href="https://m.me/panditi.neelendrareddy.7">
+                  Send a message
+                </a>
+              </div>
             </article>
             <article className="contact_option">
-              <BsWhatsapp size={35} className="social_icon" />
-              <h4>WhatsApp</h4>
-              <h5>+48739437031</h5>
-              <a href="https://api.whatsapp.com/send?+48739437031">
-                Send a message
-              </a>
+              <div className="contact_info">
+                <BsWhatsapp size={35} className="social_icon" />
+                <h4>WhatsApp</h4>
+                <h5>+48739437031</h5>
+                <a href="https://api.whatsapp.com/send?+48739437031">
+                  Send a message
+                </a>
+              </div>
             </article>
           </div>
           {/* End of Contact Optons */}
