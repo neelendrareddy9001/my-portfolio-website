a4a515a5634de228f6d5d56cc04b27ea0ff6c66c
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-03-03 05:24:45 +0100

    changes added

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Project.scss";
+import ProjectCard from "./ProjectCard";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Projects = () => {
   return (
-    <section className="contact" id="contact">
-      <h2>Hello world!</h2>
+    <section className="projects" id="projects">
+      <Container fulid className="project-section">
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works</strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard />
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
