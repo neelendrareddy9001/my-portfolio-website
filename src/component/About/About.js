ae8284b07561116de494c9cb2edee25f08744287
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-03-07 06:09:42 +0100

    changes added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -7,24 +7,62 @@ import Info from "./Info";
 const About = () => {
   return (
     <>
-      <section className="about" id="about">
-        <h2 className="section_title">About Me</h2>
-        <div className="about_container ">
-          <img src={myImg} className="about_img" alt="" />
-          <div className="about_data">
-            <Info />
-            <p className="about_description">
-              Since I am learning frontend development features. It motivates me
-              design multiple Ui templates as i kept on going.
-            </p>
-            <button
-              type="submit"
-              download=""
-              href="cv"
-              className="button button_flex"
-            >
-              Download CV
-            </button>
+      <section className="about container section" id="about">
+        <h2 className="about_title">About Me</h2>
+        <div className="about_container grid">
+          <img src={myImg} alt="" className="about_img" />
+          <div className="about_data-grid">
+            <div className="about_info">
+              <p className="about_description">
+                I am Neeledra, web developer from warszwaa Poland. I have basic
+                exprerience in web site design and buildingng and
+                customizationn, also I am good at javascript.
+              </p>
+              <a href="" className="btn">
+                Download CV
+              </a>{" "}
+            </div>
+
+            <div className="about_skills grid">
+              <div className="skills_data">
+                <div className="skills_titles">
+                  <h3 className="sills_name">Development</h3>
+                  <span className="skills_number">90%</span>
+                </div>
+                <div className="skills_bar">
+                  <span className="skills_percentage"></span>
+                </div>
+              </div>
+
+              <div className="skills_data">
+                <div className="skills_titles">
+                  <h3 className="sills_name">Development</h3>
+                  <span className="skills_number">90%</span>
+                </div>
+                <div className="skills_bar">
+                  <span className="skills_percentage"></span>
+                </div>
+              </div>
+
+              <div className="skills_data">
+                <div className="skills_titles">
+                  <h3 className="sills_name">UI/UX</h3>
+                  <span className="skills_number">70%</span>
+                </div>
+                <div className="skills_bar">
+                  <span className="skills_percentage"></span>
+                </div>
+              </div>
+              <div className="skills_data">
+                <div className="skills_titles">
+                  <h3 className="sills_name">Responsive Design</h3>
+                  <span className="skills_number">80%</span>
+                </div>
+                <div className="skills_bar">
+                  <span className="skills_percentage"></span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
