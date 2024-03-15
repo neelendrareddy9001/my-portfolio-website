2b6ac3f54c9c5eb07ad5902ba93664d60ba30a08
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-03-15 23:50:22 +0100

    changes added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+              <h1 style={{ paddingBottom: "15px" }} className="heading">
                 Hi There{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
